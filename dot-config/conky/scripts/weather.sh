93e633cc52bfd59d7fb229dbf14c233e327031ed
Shresth Prasad <shresthprasad7@gmail.com>
2025-07-18 12:47:57 +0530

    conky: take api key from .env file

--- dot-config/conky/scripts/weather.sh
+++ dot-config/conky/scripts/weather.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# This script is to get weather data from openweathermap.com in the form of a json file
-# so that conky will still display the weather when offline even though it doesn't up to date
+# This script is to get weather data from openweathermap.com in the form of a
+# json file so that conky will still display the weather when offline even
+# though it's not up to date
 
-# replace with yours
-api_key=xxxxxxxx
-# get your city id at https://openweathermap.org/find and replace
-city_id=7279746
+# Load .env
+set -a
+source ~/.config/openweather.env
+set +a
 
-url="api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&cnt=5&units=metric&lang=en"
+# Check for missing values
+[ -z "$API_KEY" ] && { echo "Missing API_KEY"; exit 1; }
+[ -z "$CITY_ID" ] && { echo "Missing CITY_ID"; exit 1; }
+
+url="api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&cnt=5&units=metric&lang=en"
 curl ${url} -s -o ~/.cache/weather.json
