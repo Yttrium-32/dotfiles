1eaa5309cc9d9ea431afd60103bb5a3b2fcc7005
Shresth Prasad <shresthprasad7@gmail.com>
2025-09-05 21:55:29 +0530

    conky: Minor formatting changes in rings script

--- dot-config/conky/scripts/rings.lua
+++ dot-config/conky/scripts/rings.lua
@@ -8,7 +8,7 @@ IMPORTANT: if you are using the 'cpu' function, it will cause a segmentation fau
 To call this script in Conky, use the following (assuming that you save this script to ~/scripts/rings.lua):
     lua_load ~/scripts/rings-v1.2.1.lua
     lua_draw_hook_pre ring_stats
-    
+
 Changelog:
 + v1.2.1 -- Fixed minor bug that caused script to crash if conky_parse() returns a nil value (20.10.2009)
 + v1.2 -- Added option for the ending angle of the rings (07.10.2009)
@@ -20,121 +20,127 @@ require('cairo_xlib')
 
 local settings_table = {
     {
-        name='cpu',
-        arg='cpu0',
-        max=100,
-        bg_colour=0xC4C4C4,
-        bg_alpha=0.2,
-        fg_colour=0x7fff00,
-        fg_alpha=0.8,
-        x=25, y=235,
-        radius=18,
-        thickness=7,
-        start_angle=0,
-        end_angle=360,
+        name = 'cpu',
+        arg = 'cpu0',
+        max = 100,
+        bg_colour = 0xC4C4C4,
+        bg_alpha = 0.2,
+        fg_colour = 0x7fff00,
+        fg_alpha = 0.8,
+        x = 25,
+        y = 235,
+        radius = 18,
+        thickness = 7,
+        start_angle = 0,
+        end_angle = 360,
     },
     {
-        name='memperc',
-        arg='',
-        max=100,
-        bg_colour=0xC4C4C4,
-        bg_alpha=0.2,
-        fg_colour=0x7fff00,
-        fg_alpha=0.8,
-        x=135, y=235,
-        radius=18,
-        thickness=7,
-        start_angle=0,
-        end_angle=360
+        name = 'memperc',
+        arg = '',
+        max = 100,
+        bg_colour = 0xC4C4C4,
+        bg_alpha = 0.2,
+        fg_colour = 0x7fff00,
+        fg_alpha = 0.8,
+        x = 135,
+        y = 235,
+        radius = 18,
+        thickness = 7,
+        start_angle = 0,
+        end_angle = 360
     },
     {
-        name='fs_used_perc',
-        arg='/',
-        max=100,
-        bg_colour=0xC4C4C4,
-        bg_alpha=0.2,
-        fg_colour=0x7fff00,
-        fg_alpha=0.8,
-        x=245, y=235,
-        radius=18,
-        thickness=7,
-        start_angle=0,
-        end_angle=360
+        name = 'fs_used_perc',
+        arg = '/',
+        max = 100,
+        bg_colour = 0xC4C4C4,
+        bg_alpha = 0.2,
+        fg_colour = 0x7fff00,
+        fg_alpha = 0.8,
+        x = 245,
+        y = 235,
+        radius = 18,
+        thickness = 7,
+        start_angle = 0,
+        end_angle = 360
     },
     {
-        name='fs_used_perc',
-        arg='/home',
-        max=100,
-        bg_colour=0xC4C4C4,
-        bg_alpha=0.2,
-        fg_colour=0x7fff00,
-        fg_alpha=0.8,
-        x=245, y=235,
-        radius=8,
-        thickness=7,
-        start_angle=0,
-        end_angle=360,
+        name = 'fs_used_perc',
+        arg = '/home',
+        max = 100,
+        bg_colour = 0xC4C4C4,
+        bg_alpha = 0.2,
+        fg_colour = 0x7fff00,
+        fg_alpha = 0.8,
+        x = 245,
+        y = 235,
+        radius = 8,
+        thickness = 7,
+        start_angle = 0,
+        end_angle = 360,
     },
 }
 
 require 'cairo'
 
-function rgb_to_r_g_b(colour,alpha)
+function rgb_to_r_g_b(colour, alpha)
     return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
 end
 
-function draw_ring(cr,t,pt)
-    local w,h=conky_window.width,conky_window.height
+function draw_ring(cr, t, pt)
+    local w, h = conky_window.width, conky_window.height
 
-    local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
-    local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
+    local xc, yc, ring_r, ring_w, sa, ea = pt['x'], pt['y'], pt['radius'], pt['thickness'], pt['start_angle'],
+        pt['end_angle']
+    local bgc, bga, fgc, fga = pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
 
-    local angle_0=sa*(2*math.pi/360)-math.pi/2
-    local angle_f=ea*(2*math.pi/360)-math.pi/2
-    local t_arc=t*(angle_f-angle_0)
+    local angle_0 = sa * (2 * math.pi / 360) - math.pi / 2
+    local angle_f = ea * (2 * math.pi / 360) - math.pi / 2
+    local t_arc = t * (angle_f - angle_0)
 
     -- Draw background ring
 
-    cairo_arc(cr,xc,yc,ring_r,angle_0,angle_f)
-    cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
-    cairo_set_line_width(cr,ring_w)
+    cairo_arc(cr, xc, yc, ring_r, angle_0, angle_f)
+    cairo_set_source_rgba(cr, rgb_to_r_g_b(bgc, bga))
+    cairo_set_line_width(cr, ring_w)
     cairo_stroke(cr)
 
     -- Draw indicator ring
 
-    cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
-    cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
+    cairo_arc(cr, xc, yc, ring_r, angle_0, angle_0 + t_arc)
+    cairo_set_source_rgba(cr, rgb_to_r_g_b(fgc, fga))
     cairo_stroke(cr)
 end
 
 function conky_ring_stats()
-    local function setup_rings(cr,pt)
-        local str=''
-        local value=0
+    local function setup_rings(cr, pt)
+        local str = ''
+        local value = 0
 
-        str=string.format('${%s %s}',pt['name'],pt['arg'])
-        str=conky_parse(str)
+        str = string.format('${%s %s}', pt['name'], pt['arg'])
+        str = conky_parse(str)
 
-        value=tonumber(str)
+        value = tonumber(str)
         if value == nil then value = 0 end
-        pct=value/pt['max']
+        pct = value / pt['max']
 
-        draw_ring(cr,pct,pt)
+        draw_ring(cr, pct, pt)
     end
 
-    if conky_window==nil then return end
-    local cs=cairo_xlib_surface_create(conky_window.display,conky_window.drawable,conky_window.visual, conky_window.width,conky_window.height)
+    if conky_window == nil then return end
+    local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual,
+        conky_window.width, conky_window.height)
 
-    local cr=cairo_create(cs)
+    local cr = cairo_create(cs)
 
-    local updates=conky_parse('${updates}')
-    update_num=tonumber(updates)
+    local updates = conky_parse('${updates}')
+    update_num = tonumber(updates)
 
-    if update_num>5 then
+    if update_num > 5 then
         for i in pairs(settings_table) do
-            setup_rings(cr,settings_table[i])
+            setup_rings(cr, settings_table[i])
         end
     end
-   cairo_surface_destroy(cs)
-  cairo_destroy(cr)
+    cairo_surface_destroy(cs)
+    cairo_destroy(cr)
 end
